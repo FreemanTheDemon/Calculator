c3d0bfb0069e86f1c8892b02e3dae41e9498c2f2
Freeman Bird <freemanbird@gmail.com>
2021-11-09 14:17:25 -0700

    Full calculator implementation working

--- calc.js
+++ calc.js
@@ -7,13 +7,27 @@ const reader = readline.createInterface({
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate? ", function(input){
 	tokens = input.split(' ');
 	
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+
+	// this statement handles different operations
+	if (mathSymbol === '+') {
+		console.log(num1 + num2);
+	} else if (mathSymbol === '-') {
+		console.log(num1 - num2);
+	} else if (mathSymbol === '*') {
+		console.log(num1 * num2);
+	} else if (mathSymbol === '/') {
+		console.log(num1 / num2);
+	} else if (mathSymbol === '√' || mathSymbol === 'sqrt') {
+		console.log(Math.sqrt(num1));
+	}
 
 	// This line closes the connection to the command line interface.
 	reader.close()
